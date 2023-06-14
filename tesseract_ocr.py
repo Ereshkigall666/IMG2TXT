01f382065e40c751367bfa8278df0d292a173f21
Ereshkigall <Ishi.thelema@gmail.com>
2023-06-14 23:35:44 +0200

    now works as a command line utility.

--- tesseract_ocr.py
+++ tesseract_ocr.py
@@ -1,25 +1,23 @@
 import io
 import sys
+from pathlib import Path
+import os
 import pytesseract
 import cv2 
 
 if __name__ == '__main__':
 	if len(sys.argv) == 3:
-		img_path = sys.argv[1]
-		outType = sys.argv[2]
-		ext = '.txt' if outType == 'txt' else '.alto'
+		img_path:str = sys.argv[1]
+		output_type:str = sys.argv[2]
 		img = cv2.imread(img_path)
-		
 		# preprocessings
 		# Lire un fichier de paramètre pour simplifier les démarches
 		# ocr
-		out_name = img_path.split('\.')[0] + ext
-		if ext == '.txt':
-			outFile = io.open(out_name, 'w')
-			txt = pytesseract.image_to_string(img)
-			outFile.write(txt)
+		img_Path = Path(img_path)
+		output_path:str = f"{os.path.join(img_Path.parent, img_Path.stem)}.{output_type}"
+		#print(output_path)
+		if output_type == '.txt':
+			(output_file := io.open(file=output_path, mode='w')).write(txt := pytesseract.image_to_string(img))
 		else: # alto
-			outFile = io.open(out_name, 'wb')
-			alto = pytesseract.image_to_alto_xml(img)
-			outFile.write(alto)
-		outFile.close()
+			(output_file := io.open(file=output_path, mode='wb')).write(alto:= pytesseract.image_to_alto_xml(img)) #type: ignore
+		output_file.close()
